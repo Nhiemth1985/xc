88ae4ba86a453ca70298f5195ac96a79d4cccc97
Márcio Pessoa <marcio.pessoa@sciemon.com>
2019-01-13 00:02:19 -0200

    Minor updates

--- scripts/xc-verify_all.sh
+++ scripts/xc-verify_all.sh
@@ -1,4 +1,20 @@
 #!/bin/sh
+# 
+# xc-verify_all.sh
+# 
+# Author: Márcio Pessoa <marcio.pessoa@gmail.com>
+# Contributors: none
+# 
+# Description:
+#   This script is used to check source code of all available devices.
+# 
+# Example: 
+#   xc-verify_all.sh
+# 
+# Change log:
+# 2019-01-12
+#         * Scrash version.
+# 
 
 for id in $(xc list --verbosity=1); do
   if ! xc verify --verbosity=0 --id "$id"; then
