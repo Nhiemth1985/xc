6374c87acaa5381a6e90956dd9beaff22ad7f4c3
Márcio Pessoa <marcio.pessoa@sciemon.com>
2019-09-08 11:49:48 -0300

    Some pylint adequations

--- tools/echo.py
+++ tools/echo.py
@@ -1,122 +1,114 @@
 """
-echo.py
-
-Author: Marcio Pessoa <marcio.pessoa@gmail.com>
-Contributors: none
-
-Description:
-Verbosity:
+---
+name: echo.py
+description: Echo package
+verbosity:
     0 Quiet (no messages)
     1 Errors
     2 Warnings
     3 Info (all messages)
     4 Device code running
+copyright: 2017-2019 Márcio Pessoa
+people:
+  developers:
+  - name: Márcio Pessoa
+    email: marcio.pessoa@gmail.com
+change-log:
+  2019-09-07
+  - version: 0.2
+    fixed: pylint friendly.
+  2017-07-08
+  - version: 0.06
+    added: debug function.
+    added: debugln function.
+  2017-05-11
+  - version: 0.05b
+    fixed: flush code output.
+  2017-04-01
+  - version: 0.04b
+    added: Version information.
+  2017-03-07
+  - version: 0.03b
+    added: colors code output.
+  2017-02-26
+  - version: 0.02b
+    added: __future__ functions, more near to addopt Python 3 style.
+  2017-02-20
+  - version: 0.01b
+    added: print() brackets to addopt Python 3 style.
+  2017-02-06
+  - version: 0.00b
+    added: First version.
+  """
 
-Change log:
-2017-07-08
-        * Version: 0.06
-        * Added: debug function.
-        * Added: debugln function.
-
-2017-05-11
-        * Version: 0.05b
-        * Bug fix: Added flush to code output.
-
-2017-04-01
-        * Version: 0.04b
-        * Improvement: Added version information.
-
-2017-03-07
-        * Version: 0.03b
-        * Added colors to code output.
-
-2017-02-26
-        * Version: 0.02b
-        * Added 'from __future__ import print_function' to be more proxy to
-          addopt Python 3 style.
-
-2017-02-20
-        * Version: 0.01b
-        * Added print() brackets to addopt Python 3 style.
-
-2017-02-06
-        * Version: 0.00b
-        * Scrach version.
-"""
-
-# from __future__ import print_function
 import sys
 from termcolor import cprint
 
 INDENT_WIDTH = 4
 
 
-class Echo(object):
+class Echo():
     """
     description:
     """
 
     @classmethod
-    def __init__(self):
-        self.version = 0.06
-        self.verbosity = a
+    def __init__(cls):
+        cls.version = 0.2
+        cls.verbosity = 0
 
     @classmethod
-    def verbose(self, level):
+    def verbose(cls, verbosity=None):
         """
         description:
         """
-        self.verbosity = level
+        if verbosity is None:
+            return int(cls.verbosity)
+        cls.verbosity = verbosity
+        return False
 
     @classmethod
-    def verbose_level(self):
-        """
-        description:
-        """
-        return int(self.verbosity)
-
-    @classmethod
-    def echo(self, string, level, indent=0):
+    def echo(cls, string, verbosity, indent=0):
         """
         description:
         """
         preffix = ''
-        for i in range(indent * INDENT_WIDTH):
+        for _ in range(indent * INDENT_WIDTH):
             preffix += ' '
-        self.__print(preffix + string, level, '')
+        cls.__print(preffix + string, verbosity, '')
 
     @classmethod
-    def echoln(self, string, level, indent=0):
+    def echoln(cls, string, verbosity, indent=0):
         """
         description:
         """
         preffix = ''
-        for i in range(indent * INDENT_WIDTH):
+        for _ in range(indent * INDENT_WIDTH):
             preffix += ' '
-        self.__print(preffix + string, level, '\n')
+        cls.__print(preffix + string, verbosity, '\n')
 
     @classmethod
-    def __print(self, string, level, trailer):
+    def __print(cls, string, verbosity, trailer):
         """
         description:
         """
-        if int(level) <= int(self.verbosity):
+        if int(verbosity) <= int(cls.verbosity):
             print(string, end=trailer)
             sys.stdout.flush()
 
 
-def verbose(level):
+def verbose(verbosity):
     """
     description:
     """
-    Echo.verbose(level)
+    Echo.verbose(verbosity)
 
 
 def level():
     """
     description:
     """
-    return Echo.verbose_level()
+    return Echo.verbose()
 
 
 def echo(string, indent=0):
