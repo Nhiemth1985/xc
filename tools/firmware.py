905cd046c1a9e993e03c1ad3e6e1e986b441cebf
Márcio Pessoa <marcio.pessoa@sciemon.com>
2019-09-10 16:31:03 -0300

    Some pylint adequations

--- tools/firmware.py
+++ tools/firmware.py
@@ -1,52 +1,50 @@
 """
-tools.py
-
-Author: Marcio Pessoa <marcio.pessoa@gmail.com>
-Contributors: none
-
-Change log:
-2019-03-05
-        * Version: 0.09
-        * Added: Verify (pylint3) support to MicroPython devices.
-
-2019-03-04
-        * Version: 0.08
-        * Added: Upload support to MicroPython devices.
-
-2019-01-08
-        * Version: 0.07
-        * Changed: Load minicom package directly.
-
-2018-11-16
-        * Version: 0.06b
-        * Changed: os.system to subprocess OS command calls.
-
-2017-11-11
-        * Version: 0.05b
-        * Added: Terminal line feed and carriage return customization.
-        * Added: Terminal local echo customization.
-
-2017-06-04
-        * Version: 0.04b
-        * Bug fix: Fixed program execution check.
-
-2017-05-08
-        * Version: 0.03b
-        * Bug fix: Removed '\n' character from self.device_path for serial
-          devices.
-
-2017-04-01
-        * Version: 0.02b
-        * Improvement: Added version information.
-
-2017-02-24
-        * Version: 0.01b
-        * Added: Check program before execution
-        * Added: Checks for arduino and miniterm.py executables on system.
-
-2016-02-13
-        * Version: 0.00b
-        * Scrach version.
+name: firmware.py
+description: Device firmeware management package
+copyright: 2016-2019 Marcio Pessoa
+people:
+  developers:
+  - name: Marcio Pessoa
+    email: marcio.pessoa@gmail.com
+  contributors:
+  - name: None
+change-log:
+  2019-03-05
+  - version: 0.1
+    fixed: pylint friendly
+  2019-03-05
+  - version: 0.09
+    added: Verify (pylint3) support to MicroPython devices.
+  2019-03-04
+  - version: 0.08
+    added: Upload support to MicroPython devices.
+  2019-01-08
+  - version: 0.07
+    changed: Load minicom package directly.
+  2018-11-16
+  - version: 0.06b
+    changed: os.system to subprocess OS command calls.
+  2017-11-11
+  - version: 0.05b
+    added: Terminal line feed and carriage return customization.
+    added: Terminal local echo customization.
+  2017-06-04
+  - version: 0.04b
+    fixed: fixed program execution check.
+  2017-05-08
+  - version: 0.03b
+    fixed: Removed '\n' character from self.device_path for serial
+  devices.
+  2017-04-01
+  - version: 0.02b
+    Improvement: added version information.
+  2017-02-24
+  - version: 0.01b
+    added: Check program before execution
+    added: Checks for arduino and miniterm.py executables on system.
+  2016-02-13
+  - version: 0.00b
+    Scrach version.
 """
 
 import sys
@@ -54,41 +52,42 @@ import os.path
 import subprocess
 from serial.tools.miniterm import Miniterm
 from tools.session import Session
-from tools.echo import verbose, level, \
-    echo, echoln, erro, erroln, warn, warnln, info, infoln, code, codeln
+import tools.echo as echo
 
 
-class DevTools:
+class DevTools:  # pylint: disable=too-many-instance-attributes
+    """
+    description:
+    """
 
     def __init__(self, data):
-        """
-        description:
-        """
-        self._version = 0.08
+        self._version = 0.1
         self.arduino_program = "arduino"
+        self.__id = None
+        self.platform = None
+        self.mark = None
+        self.description = None
+        self.architecture = None
+        self.system_path = None
+        self.system_work = None
+        self.system_code = None
+        self.system_logs = None
+        self.path = None
+        self.arduino_file = None
+        self.logs = None
+        self.device_path = None
+        self.device_speed = None
+        self.terminal_echo = False
+        self.terminal_end_of_line = 'LF'
+        self.network_address = None
+        self.device_port = None
+        self.interface = 'serial'
+        self.destination = ''
         self.load(data)
-        self.set()
+        self.__set()
 
-    def load(self, data):
-        """
-        description:
-        """
-        self.data = data
-
-    def info(self):
-        """
-        description:
-        """
-        infoln('Project...')
-        infoln('    Platform: ' + self.platform)
-        infoln('    Architecture: ' + self.architecture)
-
-    def set(self):
-        """
-        description:
-        """
-        self.reset()
-        self.id = id
+    def __set(self):
+        self.__id = None
         self.platform = self.data["system"].get("plat", self.platform)
         self.mark = self.data["system"].get("mark", self.mark)
         self.description = self.data["system"].get("desc", self.description)
@@ -119,30 +118,44 @@ class DevTools:
         elif self.interface == 'network':
             self.destination = self.network_address
 
-    def reset(self):
+    def load(self, data):
         """
         description:
         """
-        self.id = None
-        self.platform = None
-        self.mark = None
-        self.description = None
-        self.architecture = None
-        self.system_path = None
-        self.system_work = None
-        self.system_code = None
-        self.system_logs = None
-        self.path = None
-        self.arduino_file = None
-        self.logs = None
-        self.device_path = None
-        self.device_speed = None
-        self.terminal_echo = False
-        self.terminal_end_of_line = 'LF'
-        self.network_address = None
-        self.device_port = None
-        self.interface = 'serial'
-        self.destination = ''
+        self.data = data
+
+    def info(self):
+        """
+        description:
+        """
+        echo.infoln('Project...')
+        echo.infoln('    Platform: ' + self.platform)
+        echo.infoln('    Architecture: ' + self.architecture)
+
+    # def reset(self):
+        # """
+        # description:
+        # """
+        # self.__id = None
+        # self.platform = None
+        # self.mark = None
+        # self.description = None
+        # self.architecture = None
+        # self.system_path = None
+        # self.system_work = None
+        # self.system_code = None
+        # self.system_logs = None
+        # self.path = None
+        # self.arduino_file = None
+        # self.logs = None
+        # self.device_path = None
+        # self.device_speed = None
+        # self.terminal_echo = False
+        # self.terminal_end_of_line = 'LF'
+        # self.network_address = None
+        # self.device_port = None
+        # self.interface = 'serial'
+        # self.destination = ''
 
     def terminal(self):
         """
@@ -152,7 +165,7 @@ class DevTools:
         if 'linux' in self.architecture:
             ret = os.system('ssh' + ' ' + 'xc' + '@' + self.network_address)
             if ret != 0:
-                erroln("Return code: " + str(ret))
+                echo.erroln("Return code: " + str(ret))
             sys.exit(ret)
         # Connect a terminal to SSH console.
         if self.interface == 'network':
@@ -160,11 +173,11 @@ class DevTools:
                             str(self.network_address) +
                             ' telnet localhost 6571')
             if ret != 0:
-                erroln("Return code: " + str(ret))
+                echo.erroln("Return code: " + str(ret))
             sys.exit(ret)
         # Connect a terminal to device serial console.
-        infoln("Communication device: " +
-               os.popen("readlink -f " + self.device_path).read().rstrip())
+        echo.infoln("Communication device: " +
+                    os.popen("readlink -f " + self.device_path).read().rstrip())
         # Start serial session
         session = Session(self.data["comm"])
         instance = session.start()
@@ -193,36 +206,36 @@ class DevTools:
         """
         description:
         """
-        infoln('Verifying...')
+        echo.infoln('Verifying...')
         # MicroPython
         if self.architecture == "MicroPython:ARM:PYBv1.1":
             cmd = 'find ' + self.system_work + ' -name "*.py" ' + \
-                  '-exec pylint3 {} \;'
+                  '-exec pylint3 {} \;'  # pylint: disable=anomalous-backslash-in-string
         # arduino
         else:
             # Check if arduino exists
-            if self.__which(self.arduino_program) is None:
-                erroln('Program not found: ' + self.arduino_program)
+            if __which(self.arduino_program) is None:
+                echo.erroln('Program not found: ' + self.arduino_program)
                 sys.exit(True)
             # Build command
             cmd = self.arduino_program + \
                 " --verify --board " + self.architecture + " " + \
                 self.path + "/" + self.arduino_file
-        if level() < 3:
+        if echo.level() < 3:
             cmd += " >/dev/null 2>&1"
         return_code = subprocess.call(cmd, shell=True)
         if return_code != 0:
-            erroln("Return code: " + str(return_code))
+            echo.erroln("Return code: " + str(return_code))
             if return_code > 127:
                 return_code = 1
-        infoln('')
+        echo.infoln('')
         sys.exit(return_code)
 
     def upload(self):
         """
         description:
         """
-        infoln('Uploading...')
+        echo.infoln('Uploading...')
         # MicroPython
         if self.architecture == "MicroPython:ARM:PYBv1.1":
             cmd = 'rsync \
@@ -233,52 +246,53 @@ class DevTools:
                    --exclude "Pictures" \
                    ' + self.system_work + '/* ' + self.system_code + '/' + \
                    "; sync"
-            infoln("From: " + self.system_work, 1)
-            infoln("To: " + self.system_code, 1)
+            echo.infoln("From: " + self.system_work, 1)
+            echo.infoln("To: " + self.system_code, 1)
         # Arduino
         else:
             # Check if arduino program exists
-            if self.__which(self.arduino_program) is None:
-                erroln('Program not found: ' + self.arduino_program)
+            if __which(self.arduino_program) is None:
+                echo.erroln('Program not found: ' + self.arduino_program)
                 sys.exit(True)
             # Build command
             cmd = self.arduino_program + " --upload --board " + \
                 self.architecture + " " + \
                 self.path + "/" + self.arduino_file + \
                 " --port " + self.destination
-            infoln("Communication device: " + self.destination.rstrip() + ".")
-        if level() < 3:
+            echo.infoln("Communication device: " + self.destination.rstrip() + ".")
+        if echo.level() < 3:
             cmd += " >/dev/null 2>&1"
         return_code = subprocess.call(cmd, shell=True)
         if return_code == 0:
-            f = open(self.logs + "/.buildno", 'r')
-            buildno = f.read()
-            f.close()
+            file_obj = open(self.logs + "/.buildno", 'r')
+            buildno = file_obj.read()
+            file_obj.close()
             if buildno == "":
                 buildno = 1
             else:
                 buildno = int(buildno) + 1
-            f = open(self.logs + "/.buildno", 'w')
-            f.write(str(buildno))
-            infoln("Build number: " + str(buildno))
+            file_obj = open(self.logs + "/.buildno", 'w')
+            file_obj.write(str(buildno))
+            echo.infoln("Build number: " + str(buildno))
         else:
-            erroln("Return code: " + str(return_code))
+            echo.erroln("Return code: " + str(return_code))
             if return_code > 127:
                 return_code = 1
-        infoln('')
+        echo.infoln('')
         sys.exit(return_code)
 
-    def __which(self, program):
-        def is_exe(fpath):
-            return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
-        fpath, fname = os.path.split(program)
-        if fpath:
-            if is_exe(program):
-                return program
-        else:
-            for path in os.environ["PATH"].split(os.pathsep):
-                path = path.strip('"')
-                exe_file = os.path.join(path, program)
-                if is_exe(exe_file):
-                    return exe_file
-        return None
+
+def __which(program):
+    def is_exe(fpath):
+        return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
+    fpath = os.path.split(program)[0]
+    if fpath:
+        if is_exe(program):
+            return program
+    else:
+        for path in os.environ["PATH"].split(os.pathsep):
+            path = path.strip('"')
+            exe_file = os.path.join(path, program)
+            if is_exe(exe_file):
+                return exe_file
+    return None
